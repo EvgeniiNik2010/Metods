a09d9d800fe27fa10f0298b2e41923aaf022a4f9
EvgeniiNik2010 <eurostart@gmail.com>
2024-12-17 14:18:41 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
-    public static void main(String[] args) {
 
+    //Задача 1
 
+    public static void calculateLeapYear(int year) {
+        if (year > 1584 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)) {
+            System.out.println(year + " год - високосный год");
+        } else {
+            System.out.println(year + " год - невискосный год");
+        }
     }
-}
+
+
+
+    public static void main(String[] args) {
+            int year = 3005;
+        calculateLeapYear(year);
+        }
+    }
+
+    //Задача 2
+
