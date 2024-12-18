4658f369ca9e02bd89a6e03cd8cda3d32d1d7a6e
EvgeniiNik2010 <eurostart@gmail.com>
2024-12-18 12:33:54 +0700

    1

--- src/Main.java
+++ src/Main.java
@@ -26,19 +26,38 @@ public class Main {
     }
 
     //Задача 3
-    public static void calculateDeliveryDistance() {
+    public static int calculateDeliveryDistance(int km) {
 
+        if (km <= 20) {
+            return 1;
+        } else if (km > 20 && km < 60) {
+            return 2;
+        } else if (km >= 60 && km < 100) {
+            return 3;
+        } else
+            return 0;
     }
 
 
     public static void main(String[] args) {
-            int year = 3005;
+        //Задача 1
+        int year = 3005;
         calculateLeapYear(year);
 
+        //Задача 2
         int clientOS1 = 0;
         int clientDeviceYear = 2014;
         phoneSelect(clientDeviceYear, clientOS1);
+
+        //Задача 3
+        int delDist = 96;
+        int days = calculateDeliveryDistance(delDist);
+        if (days > 0) {
+            System.out.println("Потребуется дней: " + days + " срок доставки.");
+        } else {
+            System.out.println("Доставки нет.");
         }
     }
+}
 
 
